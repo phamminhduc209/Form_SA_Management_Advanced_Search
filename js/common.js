ea9ee3ed086b4edd88f409e073e434549e4d930b
Pham Minh Duc <minhducpham.it@gmail.com>
2017-09-21 16:39:04 +0700

    Duc

--- js/common.js
+++ js/common.js
@@ -1,29 +1,14 @@
 /*
  * ---------------------------------------------------
- * 1. Slide Carousel
- * 2. Scroll to Top
- * 3. Sticky Menu
- * 4. Accordion has icon
- * 5. Hover tag a show ul page Product
- * 6. POPUP order a product - check on info Payment
- * 7. Scroll News Item Tablet & Mobile
+ * 1. Scroll to Top
  */
 
   (function($){
     "use strict";
   /* ==================================================== */
 
-  $(".select2").select2({
-    placeholder: "Please choose",
-  });
-  // $(document).ready(function() {
-  //     $('.select2').each(function(index, el) {
-  //    var config = $(this).data();
-  //     });
-  // });
-
   /*
-   * 2. Scroll to Top
+   * 1. Scroll to Top
   */
   $(window).scroll(function() {
     if ($(this).scrollTop() >= 200) {
